a1c15b7ffaa213faa0d8e1cadceb9f6740228022
Harlan Howe <hhowe7@gatech.edu>
2020-08-22 20:26:40 -0500

    Revised system - added suggested variables and simplified loop.

--- src/NID.java
+++ src/NID.java
@@ -6,42 +6,40 @@ public class NID
     public static void main(String[] args)
     {
         int imageWidth, imageHeight;
-        int currentRow = 0;
-        int currentCol = 0;
         int n;
+        int currentRow = 0;
+
+        // ++++++
+        // Suggested variables....
+        int currentCol = 0;
+        // "final" keyword means "this 'variable' is a constant."
+        final String DARK_CHARACTER  = "M";
+        final String LIGHT_CHARACTER = ".";
+        String currentSymbol = LIGHT_CHARACTER;
+        // ++++++
+
+        // Get the size of the image to be drawn.
         Scanner keyboardScanner = new Scanner(System.in);
         System.out.print("Enter the width and height of the image, separated by spaces: ");
         imageWidth = keyboardScanner.nextInt();
         imageHeight = keyboardScanner.nextInt();
-
         System.out.println("You chose a ("+imageWidth+" x "+imageHeight+") image.");
 
-        // ++++
-        // Clear the carriage return from the input, so it doesn't interfere with the next line request.
-        keyboardScanner.nextLine();
-        // ++++
+        System.out.print("Enter the numbers you wish to interpret. ");
+
         while (currentRow<imageHeight)
         {
-            System.out.print("Enter the numbers you wish to interpret. ");
-            String line = keyboardScanner.nextLine();
+            n = keyboardScanner.nextInt();
+            System.out.println(n); // temporary line so you can see that n is being read. Please delete or comment out when you start writing!
 
-            Scanner lineReader = new Scanner(line);
-
-            while(lineReader.hasNextInt())
-            {
-                n = lineReader.nextInt();
-
-                System.out.println(n); // temporary line so you can see that n is being read. Please delete when you start writing!
-
-                //--------------------------------
-                // TODO: You write this!
-
-                //--------------------------------
-
-            }
+            //--------------------------------
+            // TODO: You write this!
 
 
 
+
+
+            //--------------------------------
         }
 
     }
